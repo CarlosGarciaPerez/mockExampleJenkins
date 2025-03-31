acbeb57de0c1dcd886f198297d256ca45361bee9
CarlosGarciaPerez <carlos.garci86@gmail.com>
2025-03-31 17:46:01 -0600

    Actualizacion projecto JUnit

---
+++ exampleJunitTest/src/main/java/com/testUnitarios/Main.java
@@ -0,0 +1,12 @@
+package com.testUnitarios;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        MethosExamples example = new MethosExamples();
+
+        System.out.println(example.sumar(5,5));
+    }
+}
